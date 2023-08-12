d01eed0537a0748f484ca6a1a720df3e6fc821df
Lal Ji <lalji5029@gmail.com>
2023-08-12 17:06:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package laljipackage;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        student st=new student();
+        // st.createdatabase();
+        // st.createtable();
+
+        //CRUD  Create, read, update, delete
+        // st.createdata();
+        st.readdata();
+        //  st.updatedata();
+        //  st.deletedata();
+
+
+           }
+}
